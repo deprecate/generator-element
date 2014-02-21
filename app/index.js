c0866c6b349e245bd973d3d587c2990c329f3188
Addy Osmani <addyosmani@gmail.com>
2014-02-21 13:13:30 +0000

    Adding initial yo generator output

---
+++ app/index.js
@@ -0,0 +1,56 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var ElementGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.npmInstall();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // have Yeoman greet the user
+    console.log(this.yeoman);
+
+    // replace it with a short and sweet description of your generator
+    console.log(chalk.magenta('You\'re using the fantastic Element generator.'));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.mkdir('app/templates');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = ElementGenerator;
