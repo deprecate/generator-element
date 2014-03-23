e6361f55f1bd8b6a873b1d0d70bbfba0a117d953
Zeno Rocha <zeno.rocha@liferay.com>
2014-03-23 18:47:35 -0300

    Add tests for prompt inputs

---
+++ test/test-input.js
@@ -0,0 +1,39 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var assert = require('assert');
+var validator = require('../app/validator');
+
+describe('input validation', function () {
+    describe('element name', function () {
+        it('cannot be blank', function () {
+            var actual = validator.name(''),
+                expected = validator.ERR_NAME_BLANK;
+
+            assert.equal(actual, expected);
+        });
+
+        it('needs to contain a dash', function () {
+            var actual = validator.name('foo'),
+                expected = validator.ERR_NAME_DASH;
+
+            assert.equal(actual, expected);
+        });
+
+        it('is one of the existing elements', function () {
+            var actual = validator.name('font-face'),
+                expected = validator.ERR_NAME_EXISTS;
+
+            assert.equal(actual, expected);
+        });
+    });
+
+    describe('element description', function () {
+        it('cannot be blank', function () {
+            var actual = validator.description(''),
+                expected = validator.ERR_DESCRIPTION_BLANK;
+
+            assert.equal(actual, expected);
+        });
+    });
+});
