34cfefa0fb2556db32450c56782b9f6032c8cdda
Eugenio Romano <eugenio.romano@alfresco.com>
2016-04-25 20:43:21 +0100

    closed #1 fixed test

--- test/test-creation.js
+++ test/test-creation.js
@@ -5,35 +5,27 @@
 var path = require('path');
 var helpers = require('yeoman-test');
 var assert = require('yeoman-assert');
+var os = require('os');
 
 describe('repo generator', function () {
+
     beforeEach(function (done) {
-        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-            if (err) {
-                return done(err);
-            }
-
-            done();
-        }.bind(this));
-    });
-
-    it.skip('creates expected files', function (done) {
-
-        var expected = [
-            'src/my-element.html', 'bower.json', 'package.json', 'demo/index.html', 'README.md', 'Gruntfile.js', '.editorconfig', '.gitignore'
-        ];
-
-        helpers.run(path.join( __dirname, '../app'))
-            .withOptions({'skip-install': true})
+        helpers.run(path.join(__dirname, '../app'))
+            .inDir(path.join(os.tmpdir(), './temp'))
             .withPrompts({
                 'boilerplate': 'Polymer',
                 'polymerVersion': '1.4.0',
                 'elementName': 'my-element',
                 'lifecycle': true
             })
-            .on('end', function () {
-                assert.file(expected);
-                done();
-            });
+            .withOptions({'skip-install': true})
+            .on('end', done);
     });
+
+    it('should create the required static files', function () {
+        assert.file([
+            'test/my-element-tests.html', 'src/my-element.html', 'bower.json', 'package.json', 'demo/index.html', 'README.md', 'Gruntfile.js', '.editorconfig', '.gitignore'
+        ]);
+    });
+
 });
