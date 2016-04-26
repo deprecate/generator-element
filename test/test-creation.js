590d40d2ad985a6202128aa2a5c9dd0431bed86a
Eugenio Romano <eugenio.romano@alfresco.com>
2016-04-27 00:35:15 +0100

    some tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -9,23 +9,37 @@ var os = require('os');
 
 describe('repo generator', function () {
 
-    beforeEach(function (done) {
-        helpers.run(path.join(__dirname, '../app'))
-            .inDir(path.join(os.tmpdir(), './temp'))
-            .withPrompts({
-                'boilerplate': 'Polymer',
-                'polymerVersion': '1.4.0',
-                'elementName': 'my-element',
-                'lifecycle': true
-            })
-            .withOptions({'skip-install': true})
-            .on('end', done);
-    });
+    describe('Polymer Test', function () {
 
-    it('should create the required static files', function () {
-        assert.file([
-            'test/my-element-tests.html', 'src/my-element.html', 'bower.json', 'package.json', 'demo/index.html', 'README.md', 'Gruntfile.js', '.editorconfig', '.gitignore'
-        ]);
-    });
+        beforeEach(function (done) {
+            helpers.run(path.join(__dirname, '../app'))
+                .inDir(path.join(os.tmpdir(), './temp'))
+                .withPrompts({
+                    'boilerplate': 'Polymer',
+                    'polymerVersion': '1.4.0',
+                    'elementName': 'fake-element',
+                    'lifecycle': true
+                })
+                .withOptions({'skip-install': true})
+                .on('end', done);
+        });
 
+        it('should create the required static files', function () {
+            assert.file([
+                'test/fake-element-tests.html', 'src/fake-element.html', 'bower.json', 'package.json', 'demo/index.html', 'README.md', 'Gruntfile.js', '.editorconfig', '.gitignore'
+            ]);
+        });
+
+        it('fills the README with project data', function () {
+            assert.fileContent('README.md', 'fake-element');
+        });
+
+        it('fills the index html with project data', function () {
+            assert.fileContent('src/fake-element.html', 'fake-element');
+        });
+
+        it('fills the bower with the correct library', function () {
+            assert.fileContent('bower.json', '"polymer": "Polymer/polymer#^1.4.0"');
+        });
+    });
 });
